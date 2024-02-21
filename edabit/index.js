8399558ea52d7d7388bfe434c330e3107655544a
Bekzod <bekzodisakov18@gmail.com>
2024-02-21 13:39:11 +0500

    solve challenge

---
+++ edabit/index.js
@@ -0,0 +1,33 @@
+// challenge 1
+// https://edabit.com/challenge/4gzDuDkompAqujpRi
+// Add up the Numbers from a Single Number
+
+// loop - O(n)
+// function addUp(n) {
+//   let sum = 0;
+
+//   for (let i = 0; i <= n; i++) {
+//     sum += i;
+//   }
+
+//   return sum;
+// }
+
+// O(1)
+// const addUp = (n) => n * (n + 1) / 2
+
+// recursion
+// function addUp(n) {
+//   if (n === 1) return 1;
+//   return n + addUp(n - 1);
+// }
+
+// console.log(addUp(4));
+
+// challenge 2
+// https://javascript.info/recursion#calculate-factorial
+// Calculate factorial
+
+// challenge 3
+// https://edabit.com/challenge/tYHkTdFrEmWfxpPKF
+// Matchstick Houses
